9dff59ba16d02b69bbf0b0698a1f71e0fbc411e6
Juan <juanstory@gmail.com>
2018-01-23 01:35:31 -0300

    Clean AlarmRun.java

--- android/src/main/java/com/ioddly/alarms/AlarmRun.java
+++ android/src/main/java/com/ioddly/alarms/AlarmRun.java
@@ -12,58 +12,70 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.WritableMap;
 
 public class AlarmRun extends BroadcastReceiver {
-	/**
-	 * Fires alarm after ReactContext has been obtained
-	 * @param reactContext
-	 * @param alarmName
-	 */
-	public static void emitJSAlarmEvent(ReactContext reactContext, final String alarmName) {
-		if(reactContext.hasActiveCatalystInstance()) {
-			Log.i("RNAlarms", "Firing alarm '" + alarmName + "'");
-			WritableMap map = Arguments.createMap();
-			map.putString("alarmName", alarmName);
-			reactContext.getJSModule(AlarmEmitter.class).emit("alarmFired", map);
-		} else {
-			Log.i("RNAlarms", "no active catalyst instance; not firing alarm '" + alarmName + "'");
-		}
-	}
+	
+	public static String BOOT_EVENT = "@boot";
+	public static String ALARM_FIRED_EVENT = "alarmFired";
+
+	private String alarmName;
+	private ReactInstanceManager reactManager;
+	private ReactContext reactContext;
 
 	@Override
 	public void onReceive(final Context context, Intent intent) {
+		this.alarmName = intent.hasExtra("name") ? intent.getStringExtra("name") : BOOT_EVENT;
+		this.reactManager = getReactManager(context);
+		this.reactContext = reactManager.getCurrentReactContext();
 		if(isAlarmIntent(intent)) AlarmHelper.launchMainActivity(context);
-		prepareJSListeners(context, intent);
+		prepareJSListeners();
+	}
+
+	public void onReactContextInitialized(ReactContext reactContext) {
+		this.reactContext = reactContext;
+		emitJSAlarmEvent();
+	}
+
+	private ReactInstanceManager getReactManager(final Context context) {
+		ReactApplication reactApp = ((ReactApplication) context.getApplicationContext());
+		return reactApp.getReactNativeHost().getReactInstanceManager();
 	}
 
 	private boolean isAlarmIntent(Intent intent) {
 		return !intent.getAction().contains("android.intent.action");
 	}
 
-	private void prepareJSListeners(final Context context, Intent intent) {
-		final String alarmName = intent.hasExtra("name") ? intent.getStringExtra("name") : "@boot";
-		ReactApplication reactApp = ((ReactApplication) context.getApplicationContext());
-		ReactInstanceManager reactManager = reactApp.getReactNativeHost().getReactInstanceManager();
-		ReactContext reactContext = reactManager.getCurrentReactContext();
-		if(reactContext != null) {
-			Log.i("RNAlarms", "Attempting to fire alarm '" + alarmName + "'");
-			emitJSAlarmEvent(reactContext, alarmName);
-		} else {
-			addReactNativeInitializedListener(reactManager, alarmName);
-			createReactContextIfNecessary(reactManager);
+	private void prepareJSListeners() {
+		if(this.reactContext != null) emitJSAlarmEvent();
+		else {
+			addReactNativeInitializedListener();
+			createReactContextIfNecessary();
 		}
 	}
 
-	private void addReactNativeInitializedListener(ReactInstanceManager reactManager, final String alarmName) {
-		Log.i("RNAlarms", "Application is closed; attempting to launch and fire alarm '" + alarmName + "'");
-		reactManager.addReactInstanceEventListener(new ReactInstanceManager.ReactInstanceEventListener() {
+	private void emitJSAlarmEvent() {
+		if(this.reactContext.hasActiveCatalystInstance()) {
+			Log.i("RNAlarms", "Firing alarm '" + this.alarmName + "'");
+			this.reactContext.getJSModule(AlarmEmitter.class).emit(ALARM_FIRED_EVENT, createEventMap());
+		} else Log.i("RNAlarms", "no active catalyst instance; not firing alarm '" + this.alarmName + "'");
+	}
+
+	private WritableMap createEventMap() {
+		WritableMap map = Arguments.createMap();
+		map.putString("alarmName", this.alarmName);
+		return map;
+	}
+
+	private void addReactNativeInitializedListener() {
+		Log.i("RNAlarms", "Application is closed; attempting to launch and fire alarm '" + this.alarmName + "'");
+		final AlarmRun self = this;
+		this.reactManager.addReactInstanceEventListener(new ReactInstanceManager.ReactInstanceEventListener() {
 			public void onReactContextInitialized(ReactContext reactContext) {
-				Log.i("RNAlarms", "Attempting to fire alarm '" + alarmName + "'");
-				emitJSAlarmEvent(reactContext, alarmName);
+				self.onReactContextInitialized(reactContext);
 			}
 		});
 	}
 
-	private void createReactContextIfNecessary(ReactInstanceManager reactManager) {
-		if(!reactManager.hasStartedCreatingInitialContext()) reactManager.createReactContextInBackground();
+	private void createReactContextIfNecessary() {
+		if(!this.reactManager.hasStartedCreatingInitialContext()) this.reactManager.createReactContextInBackground();
 	}
 
 }
